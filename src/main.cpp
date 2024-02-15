85354343b708d6b696fe3e6c677c0c5d0cd998a4
Ggorets0dev <nikgorets4work@gmail.com>
2024-02-15 07:14:22 +0300

    v0.5.1: Added comestic fixes in entry point, reserved options for future operations

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,20 @@ std::string ConnectionProfile::dir_path = "./profiles";
 
 const option longOptions[] = {
     { "version", no_argument, nullptr, 'v' },
-    { "create-profile", required_argument, nullptr, 'c' },
-    { "display-profile", required_argument, nullptr, 'd' },
+    { "help", no_argument, nullptr, 'v' },
+    { "create", required_argument, nullptr, 'c' },
+    { "show", required_argument, nullptr, 's' },
+    { "execute", required_argument, nullptr, 'e' },
+    { "remove", required_argument, nullptr, 'r' },
+    { "destination", required_argument, nullptr, 'd' },
     { nullptr, 0, nullptr, 0 }
 };
 
-void createConnectionProfile(char* profile_name)
+void createProfile(char* profile_name)
 {
     // SECTION - Create required variables
-    std::string address; address.reserve(15); // IPv4 length
-    std::string details;
-    int port;
-    int buffer_size;
+    std::string buffer_string; buffer_string.reserve(15); // IPv4 length
+    int buffer_integer;
 
     ConnectionProfile new_profile(profile_name);
     // !SECTION
@@ -35,28 +37,28 @@ void createConnectionProfile(char* profile_name)
     std::cout << "Fill in the following profile information..." << std::endl;
 
     std::cout << "Device IPv4 address: ";
-    std::cin >> address;
+    std::cin >> buffer_string;
 
-    if (!new_profile.trySetAddress(address))
+    if (!new_profile.trySetAddress(buffer_string))
         return Logger::getInstance()->logError("IPv4 format of the device address is not respected ");
 
     std::cout << "Device port: ";
-    std::cin >> port;
+    std::cin >> buffer_integer;
 
-    if (std::cin.fail() || !new_profile.trySetPort(port))
+    if (std::cin.fail() || !new_profile.trySetPort(buffer_integer))
         return Logger::getInstance()->logError("An incorrect value was entered for the port value when creating the profile");
 
     std::cout << "Profile details: ";
     std::cin.ignore();
-    std::getline(std::cin, details);
+    std::getline(std::cin, buffer_string);
 
-    if (!new_profile.trySetDetails(details))
+    if (!new_profile.trySetDetails(buffer_string))
         return Logger::getInstance()->logError("An incorrect value was entered for the details value when creating the profile");
 
     std::cout << "Connection buffer size: ";
-    std::cin >> buffer_size;
+    std::cin >> buffer_integer;
 
-    if (std::cin.fail() || !new_profile.trySetBufferSize(buffer_size))
+    if (std::cin.fail() || !new_profile.trySetBufferSize(buffer_integer))
         return Logger::getInstance()->logError("An incorrect value was entered for the buffer size value when creating the profile");
     // !SECTION
 
@@ -68,22 +70,27 @@ void createConnectionProfile(char* profile_name)
     new_profile.print();
 }
 
+void removeProfile(char* profile_name)
+{
+
+}
+
 void displayProfile(char* profile_name)
 {
-    std::vector<std::string> profiles_filepaths;
+    std::vector<std::string> profiles_paths;
 
     if (strcmp(profile_name, "all") == 0)
-        profiles_filepaths = getFilePaths(ConnectionProfile::dir_path);
+        profiles_paths = getFilePaths(ConnectionProfile::dir_path);
     else
-        profiles_filepaths.push_back(profile_name);
+        profiles_paths.push_back(profile_name);
 
-    if (profiles_filepaths.empty())
+    if (profiles_paths.empty())
     {
         std::cout << "No saved profiles are available" << std::endl;
         return;
     }
 
-    for (auto& path : profiles_filepaths)
+    for (auto& path : profiles_paths)
     {
         auto current = ConnectionProfile::readFile(path);
 
@@ -97,7 +104,7 @@ void displayProfile(char* profile_name)
     }
 }
 
-void printProjectInformation()
+void printSoftwareInformation()
 {
     std::cout << "AirDuct v" << g_version << std::endl;
     std::cout << "Developer: " << g_developer << std::endl;
@@ -111,28 +118,28 @@ int main(int argc, char** argv)
     WebServer server(5632, 1000);
 
     int option, optinx;
-    while ((option = getopt_long(argc, argv, "d:c:sv", longOptions, &optinx)) != -1)
+    while ((option = getopt_long(argc, argv, "e:d:c:r:s:vh", longOptions, &optinx)) != -1)
     {
         switch (option)
         {
         case 'v':
-            printProjectInformation();
-            break;
-
-        case 's':
-            server.testNet();
-            break;
-
-        case 'd':
-            displayProfile(optarg);
+            printSoftwareInformation();
             break;
 
         case 'c':
-            createConnectionProfile(optarg);
+            createProfile(optarg);
+            break;
+
+        case 'r':
+            removeProfile(optarg);
+            break;
+
+        case 's':
+            displayProfile(optarg);
             break;
 
         default:
-            std::cerr << "Unknown option: " << static_cast<char>(option) << std::endl;
+            Logger::getInstance()->logError("Unrecognized argument received");
             return 1;
         }
     }
